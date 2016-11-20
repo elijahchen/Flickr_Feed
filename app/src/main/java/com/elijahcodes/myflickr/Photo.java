80ad86105ea54b682f8dee4d44dcf26dbf197d8e
elijahchen <code.elijahchen@gmail.com>
2016-11-20 17:12:01 -0500

    - Fixed bug with toolbar not showing/working as intended

--- app/src/main/java/com/elijahcodes/myflickr/Photo.java
+++ app/src/main/java/com/elijahcodes/myflickr/Photo.java
@@ -8,6 +8,8 @@ import java.io.Serializable;
 
 public class Photo implements Serializable{
 
+    private static final long serialVersionUID = 1L;
+
     private String mTitle;
     private String mAuthor;
     private String mAutherId;
