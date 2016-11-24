6fcbb6e1d9e7c8fdcd3df24ee80bf2e398b2b39e
elijahchen <code.elijahchen@gmail.com>
2016-11-23 22:26:25 -0500

    - Implemented search function - Troubleshooting search function

--- app/src/main/java/com/elijahcodes/myflickr/SearchActivity.java
+++ app/src/main/java/com/elijahcodes/myflickr/SearchActivity.java
@@ -1,15 +1,54 @@
 package com.elijahcodes.myflickr;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
 
 public class SearchActivity extends BaseActivity {
 
+    private static final String TAG = "SearchActivity";
+    private SearchView mSearchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        Log.d(TAG, "onCreate: Ends");
         activateToolbar(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        mSearchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        mSearchView.setSearchableInfo(searchableInfo);
+
+        mSearchView.setIconified(false);
+        
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit: Fired");
+                finish();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        Log.d(TAG, "onCreateOptionsMenu: Returned: " + true);
+        return true;
+    }
 }
